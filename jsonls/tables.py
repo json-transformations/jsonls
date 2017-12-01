a3bb66e10e2592d593d43a9e13b1622c2d84002e
Brian Peterson <bpeterso2000@yahoo.com>
2017-11-30 21:58:47 -0500

    Remove terminaltables dependency

--- jsonls/tables.py
+++ jsonls/tables.py
@@ -2,18 +2,8 @@ from collections import namedtuple
 from functools import partial
 
 import click
-from colorclass import Color, Windows
-from terminaltables import SingleTable
-
 
 Colors = namedtuple('Color', ['num', 'text'])
-
-# Windows.enable(auto_colors=True, reset_atexit=True)
-
-
-def autocolor(text, color):
-    fmt = '{{auto{color}}}{text}{{/auto{color}}}'
-    return Color(fmt.format(color=color, text=text))
 
 
 def set_color(color='white'):
@@ -34,26 +24,9 @@ def color_table(rows, colors=None):
     return result
 
 
-def draw3columns(rows, colors=Colors(num='yellow', text=('blue', 'white'))):
-    colors = (colors.num, *colors.text)
-    row_colors = (zip(row, colors) for row in rows)
-    table_data = [[autocolor(*col) for col in row] for row in row_colors]
-    return SingleTable(table_data).table
-
-
 def number_lines(s):
     return ((str(n) + '.', i) for n, i in enumerate(s, 1))
 
 
 def number_tuples(s):
     return ((str(n) + '.', *t) for n, t in enumerate(s, 1))
-
-
-'''
-def number_lines(d, color=Colors(num='yellow', text='white')):
-    padding = len(str(len(d)))
-    numbers = (str(i).rjust(padding) for i in range(1, len(d) + 1))
-    nums = map(partial(click.style, fg=color.num), numbers)
-    data = map(partial(click.style, fg=color.text), d)
-    return (n + ' ' + i for n, i in zip(nums, data))
-'''
